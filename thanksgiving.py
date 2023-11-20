d53f31767f4166d63254e2706ec7405bf6d31f96
Ben-Nennert <bn111706@wgcloud.org>
2023-11-20 10:28:55 -0600

    code

---
+++ thanksgiving.py
@@ -0,0 +1,14 @@
+#tukery cooking program
+cooking = input("would you like to calculate your turkey cook time")
+if cooking == "yes" or cooking == "Yes":
+    turkey = input("is the turkey stuffed or unstuffed?:")
+    if turkey == "stuffed" or turkey == "Stuffed":
+        weight = int(input("How much does the turkey weight?:"))
+        total_cook = weight*13
+        print(str(total_cook)  + str("minutes at 350 farenheight"))
+    elif turkey == "unstuffed" or turkey == "Unstuffed":
+        weight = int(input("How much does the turkey weight?:"))
+        total_cook = weight*15
+        print(str(total_cook)  + str("minutes at 350 farenheight"))
+
+else:
